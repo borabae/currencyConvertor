ca89fcf76a552b90b6b57c9eb082deb515a04e90
borabae <karisadove@gmail.com>
2024-05-09 14:46:39 +1000

    Function testing

--- currencyConvertor.py
+++ currencyConvertor.py
@@ -1,7 +1,13 @@
+# imported python currency conversion library
 from forex_python.convertor import CurrencyRates
 
+# function to perform currency conversion
 def convertCurrency(amount, fromCurrency, toCurrency):
     c = CurrencyRates()
     convertedAmount = c.convert(fromCurrency, toCurrency, amount)
     return convertedAmount
+
+# function testing
+convertedAmount = convertCurrency(100, 'USD', 'EUR')
+print(convertedAmount)
 
